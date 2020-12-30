0f1d386fcbaeccba070637ef91b1c9740324f6cf
gusandbud <kostisthomaidis@hotmail.com>
2020-12-30 05:00:14 +0200

    Initial commit

---
+++ src/main/java/com/kthomaidis/minmatscost/MinMatsCostApplication.java
@@ -0,0 +1,13 @@
+package com.kthomaidis.minmatscost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinMatsCostApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MinMatsCostApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kthomaidis/minmatscost/MinMatsCostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kthomaidis.minmatscost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinMatsCostApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
